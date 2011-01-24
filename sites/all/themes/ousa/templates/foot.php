b3c3db951697d43865452ea1b2458fbbadaaeecf
Pat Dunlavey <patdunlavey@gmail.com>
2011-01-24 11:18:00 -0500

    removed map image from foot area - css styling (background image) will be used for this purpose

--- sites/all/themes/ousa/templates/foot.php
+++ sites/all/themes/ousa/templates/foot.php
@@ -1,10 +1,12 @@
 <div id="foot" class="holder"><div id="foot-in" class="sector">
 
-</div></div><!--foot-->
 
 <div class="clearboth"></div>
 
-<div id="map" class="holder"><img src="/sites/all/themes/ousa/images/global/map.jpg" height="267" width="2085"  /></div>
+<div id="map" class="holder">
+<?php // <img src="/sites/all/themes/ousa/images/global/map.jpg" height="267" width="2085"  /> ?>
+</div>
+</div></div><!--foot-->
 
 <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
 <script type="text/javascript" src="http://twitter.com/statuses/user_timeline/orienteeringusa.json?callback=twitterCallback2&amp;count=5"></script>
